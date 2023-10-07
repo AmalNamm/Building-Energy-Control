f3988f40477287e08b6655bb63dfec9451a6402e
amal_nammouchi <70199-amal_nammouchi@users.noreply.gitlab.aicrowd.com>
2023-10-07 18:48:10 +0000

    Changes for submission-Submission_SACreward

--- rewards/comfort_reward.py
+++ rewards/comfort_reward.py
@@ -1,6 +1,8 @@
-from citylearn.reward_function import ComfortReward
+#from citylearn.reward_function import ComfortReward
+from citylearn.reward_function import IndependentSACReward
+#class ComfortRewardFunction(ComfortReward):
+class ComfortRewardFunction(IndependentSACReward):
 
-class ComfortRewardFunction(ComfortReward):
     """ Simple passthrough example of comfort reward from Citylearn env """
     def __init__(self, env_metadata):
         super().__init__(env_metadata)
