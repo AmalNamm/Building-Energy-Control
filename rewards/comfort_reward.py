852be31f9fc6f98c7cba565e21da8d3b44d22d3f
amal_nammouchi <70199-amal_nammouchi@users.noreply.gitlab.aicrowd.com>
2023-10-09 14:06:37 +0000

    Changes for submission-SubmissionSAC_Combine_SolarPenaltyAndComfortReward

--- rewards/comfort_reward.py
+++ rewards/comfort_reward.py
@@ -2,6 +2,7 @@ from typing import Any, List, Mapping, Tuple, Union
 import numpy as np
 from citylearn.data import ZERO_DIVISION_PLACEHOLDER
 
+
 class RewardFunction:
     r"""Base and default reward function class.
 
@@ -133,10 +134,10 @@ class ComfortRewardFunction(RewardFunction):
                 reward = -(delta**exponent)
             
             elif lower_bound_comfortable_indoor_dry_bulb_temperature <= indoor_dry_bulb_temperature < set_point:
-                reward = 0.0 #if heating else -delta + 1.0  # +1.0 for being inside the comfort zone
+                reward = 0.0 if heating else -delta + 1.0  # +1.0 for being inside the comfort zone
 
             elif set_point <= indoor_dry_bulb_temperature <= upper_bound_comfortable_indoor_dry_bulb_temperature:
-                reward = -delta #+ 1.0 if heating else 0.0 # +1.0 for being inside the comfort zone
+                reward = -delta + 1.0 if heating else 0.0 # +1.0 for being inside the comfort zone
 
             else:
                 exponent = self.higher_exponent if heating else self.lower_exponent
@@ -144,7 +145,7 @@ class ComfortRewardFunction(RewardFunction):
                 
             reward_list.append(reward)
         
-        alpha = 0.99
+        alpha = 0.29
                 
         if self.central_agent:
             final_reward = [alpha * comfort_reward[0] + (1-alpha) * sum(reward_list)]
@@ -154,5 +155,125 @@ class ComfortRewardFunction(RewardFunction):
 
         
             
+class SolarPenaltyReward(RewardFunction):
+    """The reward is designed to minimize electricity consumption and maximize solar generation to charge energy storage systems.
 
+    The reward is calculated for each building, i and summed to provide the agent with a reward that is representative of all the
+    building or buildings (in centralized case)it controls. It encourages net-zero energy use by penalizing grid load satisfaction 
+    when there is energy in the enerygy storage systems as well as penalizing net export when the energy storage systems are not
+    fully charged through the penalty term. There is neither penalty nor reward when the energy storage systems are fully charged
+    during net export to the grid. Whereas, when the energy storage systems are charged to capacity and there is net import from the 
+    grid the penalty is maximized.
+
+    Parameters
+    ----------
+    env_metadata: Mapping[str, Any]:
+        General static information about the environment.
+    """
+
+    def __init__(self, env_metadata: Mapping[str, Any]):
+        super().__init__(env_metadata)
+
+    def calculate(self, observations: List[Mapping[str, Union[int, float]]]) -> List[float]:
+        reward_list = []
+
+        for o, m in zip(observations, self.env_metadata['buildings']):
+            e = o['net_electricity_consumption']
+            cc = m['cooling_storage']['capacity']
+            hc = m['heating_storage']['capacity']
+            dc = m['dhw_storage']['capacity']
+            ec = m['electrical_storage']['capacity']
+            cs = o.get('cooling_storage_soc', 0.0)
+            hs = o.get('heating_storage_soc', 0.0)
+            ds = o.get('dhw_storage_soc', 0.0)
+            es = o.get('electrical_storage_soc', 0.0)
+            reward = 0.0
+            reward += -(1.0 + np.sign(e)*cs)*abs(e) if cc > ZERO_DIVISION_PLACEHOLDER else 0.0
+            reward += -(1.0 + np.sign(e)*hs)*abs(e) if hc > ZERO_DIVISION_PLACEHOLDER else 0.0
+            reward += -(1.0 + np.sign(e)*ds)*abs(e) if dc > ZERO_DIVISION_PLACEHOLDER else 0.0
+            reward += -(1.0 + np.sign(e)*es)*abs(e) if ec > ZERO_DIVISION_PLACEHOLDER else 0.0
+            reward_list.append(reward)
+
+        if self.central_agent:
+            reward = [sum(reward_list)]
+        else:
+            reward = reward_list
         
+        return reward
+    
+        
+class SolarPenaltyAndComfortReward(RewardFunction):
+    """Addition of :py:class:`citylearn.reward_function.SolarPenaltyReward` and :py:class:`citylearn.reward_function.ComfortReward`.
+
+    Parameters
+    ----------
+    env_metadata: Mapping[str, Any]:
+        General static information about the environment.
+    band: float, default = 2.0
+        Setpoint comfort band (+/-).
+    lower_exponent: float, default = 2.0
+        Penalty exponent for when in cooling mode but temperature is above setpoint upper
+        boundary or heating mode but temperature is below setpoint lower boundary.
+    higher_exponent: float, default = 3.0
+        Penalty exponent for when in cooling mode but temperature is below setpoint lower
+        boundary or heating mode but temperature is above setpoint upper boundary.
+    coefficients: Tuple, default = (1.0, 1.0)
+        Coefficents for `citylearn.reward_function.SolarPenaltyReward` and :py:class:`citylearn.reward_function.ComfortReward` values respectively.
+    """
+    
+    def __init__(self, env_metadata: Mapping[str, Any], band: float = None, lower_exponent: float = None, higher_exponent: float = None, coefficients: Tuple = None):
+        self.__functions: List[RewardFunction] = [
+            SolarPenaltyReward(env_metadata),
+            ComfortRewardFunction(env_metadata, band=band, lower_exponent=lower_exponent, higher_exponent=higher_exponent)
+        ]
+        super().__init__(env_metadata)
+        self.coefficients = coefficients
+
+    @property
+    def coefficients(self) -> Tuple:
+        return self.__coefficients
+    
+    @RewardFunction.env_metadata.setter
+    def env_metadata(self, env_metadata: Mapping[str, Any]) -> Mapping[str, Any]:
+        RewardFunction.env_metadata.fset(self, env_metadata)
+
+        for f in self.__functions:
+            f.env_metadata = self.env_metadata
+    
+    @coefficients.setter
+    def coefficients(self, coefficients: Tuple):
+        coefficients = [1.0]*len(self.__functions) if coefficients is None else coefficients
+        assert len(coefficients) == len(self.__functions), f'{type(self).__name__} needs {len(self.__functions)} coefficients.' 
+        self.__coefficients = coefficients
+        
+    def get_time_of_use_multiplier(self, current_time: int) -> float:
+        """Return a price multiplier based on the current time."""
+        # Simple example: 
+        # Peak hours are between 18:00 to 22:00 with 1.5x price.
+        # Off-peak hours are all other times with 0.8x price.
+        if 18 <= current_time < 22:
+            return 1.5
+        else:
+            return 0.8
+    def calculate(self, observations: List[Mapping[str, Union[int, float]]]) -> List[float]:
+        # Assume one of the observations contains the current time (e.g., in hours).
+        current_time = observations[0].get('current_time', 12)  # Default to noon if not provided.
+        time_of_use_multiplier = self.get_time_of_use_multiplier(current_time)
+
+        reward = np.array([f.calculate(observations) for f in self.__functions], dtype='float32')
+        
+        # Apply the multiplier to the reward coming from SolarPenaltyReward
+        # Assuming SolarPenaltyReward is the first in the list of functions
+        reward[0] = reward[0] * time_of_use_multiplier
+        
+        reward = reward * np.reshape(self.coefficients, (len(self.coefficients), 1))
+        reward = reward.sum(axis=0).tolist()
+
+        return reward
+
+    #def calculate(self, observations: List[Mapping[str, Union[int, float]]]) -> List[float]:
+    #    reward = np.array([f.calculate(observations) for f in self.__functions], dtype='float32')
+    #    reward = reward*np.reshape(self.coefficients, (len(self.coefficients), 1))
+    #    reward = reward.sum(axis=0).tolist()
+
+    #    return reward
--- rewards/user_reward.py
+++ rewards/user_reward.py
@@ -1,7 +1,10 @@
-from rewards.comfort_reward import ComfortRewardFunction
+#from rewards.comfort_reward import ComfortRewardFunction
+from rewards.comfort_reward import SolarPenaltyAndComfortReward
 
 ###################################################################
 #####                Specify your reward here                 #####
 ###################################################################
 
-SubmissionReward = ComfortRewardFunction
+#SubmissionReward = ComfortRewardFunction
+
+SubmissionReward = SolarPenaltyAndComfortReward
